37248133700a845790cede62e27e3ba93a4ee2e3
Anatolii Egorov <silkroadbum@mail.ru>
2022-04-26 22:38:19 +0500

    Отрисовал элементы сортировки

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import FilterView from './view/filter-view.js';
+import SortView from './view/sort-view.js';
 import { render } from './render.js';
 
 const siteBodyElement = document.querySelector('.page-body');
 const siteFilterElement = siteBodyElement.querySelector('.trip-controls__filters');
+const siteSortElement = siteBodyElement.querySelector('.trip-events');
 
 render(new FilterView(), siteFilterElement);
+render(new SortView(), siteSortElement);
