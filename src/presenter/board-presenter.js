435fce95236659eee8491f498f35742e9467404e
Anatolii Egorov <silkroadbum@mail.ru>
2022-04-27 21:25:49 +0500

    написал код создания листа в list-vew.js, создал presenter, импортировал нужные view-файлы и функцию render

---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,10 @@
+import ListView from '../view/list-view.js';
+import SortView from '../view/sort-view.js';
+import FormCreateView from '../view/form-create-view.js';
+import FormEditView from '../view/form-edit-view.js';
+import WaypointView from '../view/waypoint-view.js';
+import { render } from '../render.js';
+
+export default class BoardPresenter {
+
+}
