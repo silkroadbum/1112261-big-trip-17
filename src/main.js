6ce2fb246b964cc2c3b158ddd9701c2beb51ad54
Anatolii Egorov <silkroadbum@mail.ru>
2022-04-27 21:02:26 +0500

    Убрал лишнее из main.js, написал код создания форм добавлению новых точек и редактирования точек

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import FilterView from './view/filter-view.js';
-import SortView from './view/sort-view.js';
 import { render } from './render.js';
 
-const siteBodyElement = document.querySelector('.page-body');
-const siteFilterElement = siteBodyElement.querySelector('.trip-controls__filters');
-const siteSortElement = siteBodyElement.querySelector('.trip-events');
+const siteFilterElement = document.querySelector('.trip-controls__filters');
 
 render(new FilterView(), siteFilterElement);
-render(new SortView(), siteSortElement);
