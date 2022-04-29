5a699158048f8fc264fdc61845296f9a6123a5ee
Anatolii Egorov <silkroadbum@mail.ru>
2022-04-29 21:15:19 +0500

    Импортировал PointsModel в точку входа и создал экземпляр класса, передал экземпляр класса в презентер

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import FilterView from './view/filter-view.js';
 import { render } from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
+import PointsModel from './model/points-model.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.page-main');
 const siteEventElement = siteMainElement.querySelector('.trip-events');
+
+const pointsModel = new PointsModel();
 const boardPresenter = new BoardPresenter();
 
 render(new FilterView(), siteFilterElement);
 
-boardPresenter.init(siteEventElement);
+boardPresenter.init(siteEventElement, pointsModel);
