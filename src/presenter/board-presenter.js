de7e9c0ca58d6d4bc2e2404e72032bbb0af880d4
Anatolii Egorov <silkroadbum@mail.ru>
2022-04-29 21:21:04 +0500

    Научил презентер получать точки из модели и передавать их в представление

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,21 +4,22 @@ import FormCreateView from '../view/form-create-view.js';
 import FormEditView from '../view/form-edit-view.js';
 import WaypointView from '../view/waypoint-view.js';
 import { render } from '../render.js';
-import { NUM_OF_POINTS } from '../const.js';
 
 export default class BoardPresenter {
   boardComponent = new BoardView();
 
-  init = (boardContainer) => {
+  init = (boardContainer, pointsModel) => {
     this.boardContainer = boardContainer;
+    this.pointsModel = pointsModel;
+    this.boardPoints = [...this.pointsModel.getPoints()];
 
     render(new SortView(), boardContainer);
     render(this.boardComponent, this.boardContainer);
     render(new FormEditView(), this.boardComponent.getElement());
     render(new FormCreateView(), this.boardComponent.getElement());
 
-    for (let i = 0; i < NUM_OF_POINTS; i++) {
-      render(new WaypointView(), this.boardComponent.getElement());
+    for (let i = 0; i < pointsModel.length; i++) {
+      render(new WaypointView(this.boardPoints[i]), this.boardComponent.getElement());
     }
   };
 }
