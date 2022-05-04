5020351208c5383de4c3fd260bac812dbf7458dd
Anatolii Egorov <silkroadbum@mail.ru>
2022-05-04 23:43:39 +0500

    Подставил моковые данные в форму редактирования

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import FilterView from './view/filter-view.js';
 import { render } from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import PointsModel from './model/points-model.js';
+import { generateDestinationData } from './mock/destination.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.page-main');
@@ -9,7 +10,8 @@ const siteEventElement = siteMainElement.querySelector('.trip-events');
 
 const pointsModel = new PointsModel();
 const boardPresenter = new BoardPresenter();
+const destinationData = generateDestinationData();
 
 render(new FilterView(), siteFilterElement);
 
-boardPresenter.init(siteEventElement, pointsModel);
+boardPresenter.init(siteEventElement, pointsModel, destinationData);
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -7,14 +7,14 @@ import { render } from '../render.js';
 export default class BoardPresenter {
   boardComponent = new BoardView();
 
-  init = (boardContainer, pointsModel) => {
+  init = (boardContainer, pointsModel, destination) => {
     this.boardContainer = boardContainer;
     this.pointsModel = pointsModel;
     this.boardPoints = [...this.pointsModel.getPoints()];
 
     render(new SortView(), boardContainer);
     render(this.boardComponent, this.boardContainer);
-    render(new FormEditView(), this.boardComponent.getElement());
+    render(new FormEditView(this.boardPoints[0], destination), this.boardComponent.getElement());
 
     for (let i = 0; i < this.boardPoints.length; i++) {
       render(new WaypointView(this.boardPoints[i]), this.boardComponent.getElement());
