32d89ca3bb4176127cd5d11d0546a93a61859564
Anatolii Egorov <silkroadbum@mail.ru>
2022-05-19 20:31:41 +0500

    Подключил моки фильтров, импортировал функцию по генерации в точке входа и передал из модели в компонент

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { render } from './framework/render.js';
 import BoardPresenter from './presenter/board-presenter.js';
 import PointsModel from './model/points-model.js';
 import { generateDestinationData } from './mock/destination.js';
+import { generateFilter } from './mock/filter.js';
 
 const siteFilterElement = document.querySelector('.trip-controls__filters');
 const siteMainElement = document.querySelector('.page-main');
@@ -11,8 +12,9 @@ const siteEventElement = siteMainElement.querySelector('.trip-events');
 const pointsModel = new PointsModel();
 const destinationData = generateDestinationData();
 const boardPresenter = new BoardPresenter(siteEventElement, pointsModel, destinationData);
+const filters = generateFilter(pointsModel.points);
 
 
-render(new FilterView(), siteFilterElement);
+render(new FilterView(filters), siteFilterElement);
 
 boardPresenter.init();
