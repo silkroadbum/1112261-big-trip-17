31e89bec023f0ff39c2bc998f81702d3cde3106f
Anatolii Egorov <silkroadbum@mail.ru>
2022-06-01 22:01:54 +0500

    Создал презентер новой точки машрута

---
+++ src/presenter/point-new-presenter.js
@@ -0,0 +1,76 @@
+import { render, remove, RenderPosition } from '../framework/render.js';
+import FormEditView from '../view/form-edit-view.js';
+import { UserAction, UpdateType } from '../const.js';
+import { nanoid } from 'nanoid';
+
+
+export default class PointPresenter {
+  #pointListContainer = null;
+  #changeData = null;
+  #pointEditComponent = null;
+  #destroyCallback = null;
+
+  #destination = null;
+  #offers = null;
+
+  constructor(pointListContainer, destination, changeData, offers) {
+    this.#pointListContainer = pointListContainer;
+    this.#destination = destination;
+    this.#offers = offers;
+    this.#changeData = changeData;
+  }
+
+  init = (callback) => {
+    this.#destroyCallback = callback;
+
+    if (this.#pointEditComponent !== null) {
+      return;
+    }
+
+    this.#pointEditComponent = new FormEditView();
+    this.#pointEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
+    this.#pointEditComponent.setClickHandler(this.#handleFormClick);
+    this.#pointEditComponent.setDeleteClickHandler(this.#handleDeleteClick);
+
+    render(this.#pointEditComponent, this.#pointListContainer, RenderPosition.AFTERBEGIN);
+
+    document.addEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+  destroy = () => {
+    if (this.#pointEditComponent === null) {
+      return;
+    }
+
+    this.#destroyCallback?.();
+
+    remove(this.#pointEditComponent);
+    this.#pointEditComponent = null;
+
+    document.removeEventListener('keydown', this.#escKeyDownHandler);
+  };
+
+  #escKeyDownHandler = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      this.destroy();
+    }
+  };
+
+  #handleFormSubmit = (point) => {
+    this.#changeData(
+      UserAction.ADD_POINT,
+      UpdateType.MINOR,
+      { id: nanoid(), ...point },
+    );
+    this.destroy();
+  };
+
+  #handleFormClick = () => {
+    this.destroy();
+  };
+
+  #handleDeleteClick = () => {
+    this.destroy();
+  };
+}
