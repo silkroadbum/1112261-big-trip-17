868e36d3abc9fc7faf4d2843438b0939635274ea
Anatolii Egorov <silkroadbum@mail.ru>
2022-06-05 19:09:11 +0500

    Удалил ненужные константы

--- src/const.js
+++ src/const.js
@@ -1,14 +1,6 @@
-const DESTINATIONS = ['Amsterdam', 'Chamonix', 'Geneva'];
 const TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 const AUTHORIZATION = 'Basic h2oj7mnk99';
 const END_POINT = 'https://17.ecmascript.pages.academy/big-trip/';
-const DESCRIPTION_OF_DESTINATION = [
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-  'Cras aliquet varius magna, non porta ligula feugiat eget.',
-  'Fusce tristique felis at fermentum pharetra.',
-  'Aliquam id orci ut lectus varius viverra.',
-  'Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.'
-];
 const MODE = {
   DEFAULT: 'DEFAULT',
   EDITING: 'EDITING',
@@ -51,4 +43,4 @@ const Method = {
   PUT: 'PUT',
 };
 
-export { DESTINATIONS, TYPES, DESCRIPTION_OF_DESTINATION, MODE, SORT_TYPE, FILTER_TYPE, UserAction, UpdateType, BLANK_POINT, Method, AUTHORIZATION, END_POINT };
+export { TYPES, MODE, SORT_TYPE, FILTER_TYPE, UserAction, UpdateType, BLANK_POINT, Method, AUTHORIZATION, END_POINT };
